ca5ad467fcb1bed8e56f0a60ffc9d77323bbbe1e
Nickita0000 <nzadubniak@gmail.com>
2023-03-27 00:48:13 +0100

    Promise

---
+++ index.js
@@ -0,0 +1,42 @@
+const inputUserName = document.querySelector('#user_name')
+const button = document.querySelector('#btn_confirm')
+const container = document.querySelector('#container')
+
+button.addEventListener('click', onButtonConfirmClick)
+
+function onButtonConfirmClick(){
+    const userName = getUserName()
+    clearInput()
+    fetch(` https://api.github.com/users/${userName.login}`)
+        .then((response) => {
+            return response.json()
+        })
+        .then((user) => {
+            console.log(user)
+            renderUserData(user)
+        })
+}
+
+function getUserName() {
+    return {
+        login: inputUserName.value
+    }
+}
+
+function htmlUserData(user) {
+    return `
+        <span>Login: ${user.login}</span><br><br>
+        <img src="${user.avatar_url}" alt="Avatar"><br><br>
+        <span>Repos: ${user.public_repos}</span><br><br>
+        <span>Followers: ${user.followers}</span><br><br>
+        <span>Following: ${user.following}</span><br><br>
+    `
+}
+
+function renderUserData(user) {
+    return container.innerHTML = htmlUserData(user)
+}
+
+function clearInput() {
+    inputUserName.value = ''
+}
